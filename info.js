96c4aa19fa57ff0ae1d317f5e3d4e5a40d8bf6bb
supportjoxicraft <support@joxicraft.co.za>
2023-03-03 15:43:12 +0200

    Tasks

---
+++ info.js
@@ -0,0 +1,13 @@
+let firstName = "Dineo"
+let lastName = "Moroka";
+let bio = "I'm a graduate from Sol Plaatje University with a Bachelor's degree in Data Science. Born and raised in Kimberley, South Africa,I have a passion for solving complex problems and utilizing data to drive positive outcomes.During my time at Sol Plaatje University, I gained hands-on experience in data analysis, machine learning, and big data technologies. I am currently an intern at Joxicraft, where I am applying my skills and knowledge to support data - driven decision making for the company.With my strong academic background and eagerness to learn, I am poised to make a valuable contribution to the tech industry."
+
+// Define variables for educational background
+let degree = "Bachelor of Science in Data Science";
+let university = "Sol Plaatje University";
+let year = "2021";
+
+let email = "dmoroka11@gmail.com";
+let phone = "0814044131";
+let address = "7th Avenue Homevale Kimberley 8301";
+
